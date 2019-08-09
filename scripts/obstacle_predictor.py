ef269885ef4b785d81fcd1158a5d28ffb92b4a6d
Jaehyun Lim <lim.yonsei@gmail.com>
2019-08-10 06:02:19 +0900

    Create utils.py and cleanup code.

--- scripts/obstacle_predictor.py
+++ scripts/obstacle_predictor.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 from copy import copy
-from scipy import signal, interpolate
 from scipy.ndimage.filters import gaussian_filter
 
 import rospy
@@ -15,7 +14,9 @@ except:
     rospy.logerr('Failed to import ObstacleArrayMsg, ObstacleMsg.')
 
 import numpy as np
-import cv2
+from cv2 import resize, calcOpticalFlowFarneback
+
+from utils import *
 
 
 
@@ -113,7 +114,7 @@ class ObstaclePredictor:
                 I2g = gaussian_filter(I2g, sigma=0.5)
                 # flow, rep_physics = opticalFlowLK(I2g, I1g, self.window_size)
                 # flow = -flow
-                flow = -cv2.calcOpticalFlowFarneback(I2g, I1g, None, 0.5, 2, self.window_size, 3, 5, 1.2, 0)
+                flow = -calcOpticalFlowFarneback(I2g, I1g, None, 0.5, 2, self.window_size, 3, 5, 1.2, 0)
                 flow[:,:,0] = gaussian_filter(flow[:,:,0], sigma=5)
                 flow[:,:,1] = gaussian_filter(flow[:,:,1], sigma=5)
         
@@ -172,7 +173,7 @@ class ObstaclePredictor:
         di = int(round(dy/self.global_costmap_msg.info.resolution))
         dj = int(round(dx/self.global_costmap_msg.info.resolution))
 
-        mask = cv2.resize(
+        mask = resize(
             self.global_costmap_msg.data[
                 di:di+int(round(costmap_msg.info.height*multiplier)),
                 dj:dj+int(round(costmap_msg.info.width*multiplier))
@@ -216,88 +217,6 @@ class ObstaclePredictor:
 
 ##  End of class ObstaclePredictor.
 
-
-
-def opticalFlowLK(I1g, I2g, window_size, tau=1e-2): # 7.31 add
-    '''
-    Lucas-Kanade optical flow
-    '''
-    kernel_x = np.array([[-1., 1.], [-1., 1.]])
-    kernel_y = np.array([[-1., -1.], [1., 1.]])
-    kernel_t = np.array([[1., 1.], [1., 1.]])#*.25
-    w = window_size / 2 # window_size is odd, all the pixels with offset in between [-w, w] are inside the window
-    I1g = I1g / 255. # normalize pixels
-    I2g = I2g / 255. # normalize pixels
-    # Implement Lucas Kanade
-    # for each point, calculate I_x, I_y, I_t
-    mode = 'same'
-    fx = signal.convolve2d(I1g, kernel_x, boundary='symm', mode=mode)
-    fy = signal.convolve2d(I1g, kernel_y, boundary='symm', mode=mode)
-    ft = signal.convolve2d(I2g, kernel_t, boundary='symm', mode=mode) + signal.convolve2d(I1g, -kernel_t, boundary='symm', mode=mode)
-    uv = np.zeros([I1g.shape[0], I1g.shape[1], 2])
-    # within window window_size * window_size
-    rep_x_list = []
-    rep_y_list = []
-    u_list = []
-    v_list = []
-    for i in range(w, I1g.shape[0]-w):
-        for j in range(w, I1g.shape[1]-w):
-            Ix = fx[i-w:i+w+1, j-w:j+w+1].flatten()
-            Iy = fy[i-w:i+w+1, j-w:j+w+1].flatten()
-            It = ft[i-w:i+w+1, j-w:j+w+1].flatten()
-            b = -It # Av = b <- Ix*u+Iy*v+It = 0
-            A = np.stack([Ix, Iy], axis = 1) # 8.1 add. Ix & Iy are columns.
-            # if threshold τ is larger than the smallest eigenvalue of A'A:
-            nu = np.matmul(np.linalg.pinv(A), b) # v = (A'A)^-1 * (-A'It)
-            if 0.3<np.linalg.norm(nu)<1.1:
-                uv[i, j, 0] = nu[0]
-                uv[i, j, 1] =-nu[1]
-                u_list.append(nu[0])
-                v_list.append(nu[1])
-                rep_x_list.append(j)
-                rep_y_list.append(i)
-                #print(i,j)
-
-    rep_x = np.average(rep_x_list)
-    rep_y = np.average(rep_y_list)
-    rep_u = np.average(u_list)
-    rep_v = np.average(v_list)
- 
-    return uv, [rep_x,rep_y,rep_u,-rep_v]   # [August 8, 19:46] Return changed: (ndarray(nx,ny), ndarray(nx,ny)) -> ndarray(nx,ny,2) 
-
-
-def isOccupancyGrid(msg):
-    '''
-    Return False if msg is not an OccupancyGrid class.
-    '''
-    return type(msg) == type(OccupancyGrid())
-
-
-def isOccupancyGridUpdate(msg):
-    '''
-    Return False if msg is not an OccupancyGridUpdate class.
-    '''
-    return type(msg) == type(OccupancyGridUpdate())
-
-
-def reshapeCostmap(msg):
-    '''
-    Reshape, remove negative values and change type as uint8 (for cv2.resize)
-    '''
-    msg.data = np.reshape(
-        msg.data,
-        [msg.info.height, msg.info.width]
-    ).clip(0).astype(np.uint8)
-    return msg
-
-
-def updateCostmap(costmap_msg, update_msg):
-    temp = copy(costmap_msg)
-    temp.header.stamp = update_msg.header.stamp
-    temp.info.width = update_msg.width
-    temp.info.height = update_msg.height
-    temp.data = update_msg.data
-    costmap_msg = reshapeCostmap(temp)
 
 
 if __name__ == '__main__':
